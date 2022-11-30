2b4e693473023a53dadfa8d85147b5000dc886bd
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2022-11-30 14:51:06 +0200

    Appearence site sections added

--- script.js
+++ script.js
@@ -132,9 +132,28 @@ const getStickyNav = function (entries) {
     nav.classList.remove('sticky');
   }
 };
-const observer = new IntersectionObserver(getStickyNav, {
+const headerObserver = new IntersectionObserver(getStickyNav, {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`, // ("-100px" або шаблонну строку) якщо потрібен відступ до границі
 });
-observer.observe(header);
+headerObserver.observe(header);
+
+// Appearence site sections
+const allSections = document.querySelectorAll('.section');
+
+const appearenceSection = function (entries, observer) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(appearenceSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
